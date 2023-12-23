1bb2709f98091c89b9418a3632ed9d6ba54b5d66
my23aba <154661430+my23aba@users.noreply.github.com>
2023-12-23 14:23:31 +0500

    Add files via upload

---
+++ src/model/User.java
@@ -0,0 +1,13 @@
+package model;
+
+public class User {
+    private String role;
+
+    public User(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
